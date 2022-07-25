380b97946eea6090a8a94ada0ca4589ab4481d7b
Manish R Jain <manishrjain@gmail.com>
2022-07-24 21:53:04 -0700

    Fix up the edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/outcaste-io/outserv',
+          editUrl: 'https://github.com/outcaste-io/docs/blob/main',
         },
         // blog: {
         //   showReadingTime: true,
